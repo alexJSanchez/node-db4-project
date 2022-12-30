6938690daa02ef3d5d28792eb6ac718a92a404bb
alex sanchez <alexjsanchez1020@gmail.com>
2022-12-30 09:46:30 -0500

    data base set up

--- data/db-config.js
+++ data/db-config.js
@@ -1,6 +1,7 @@
 const knex = require('knex')
+
 const configurations = require('../knexfile.js')
 
-const enviornment = process.env.NODE_ENV
+const enviornment = process.env.NODE_ENV || 'development'
 
 module.exports = knex(configurations[enviornment])
--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,20 @@
 const sharedConfig = {
     client : 'sqlite3',
     migrations: {directory: './data/migrations'},
+    seeds: {directory: './data/seeds'},
+    //sqlite3
     useNullAsDefault: true,
-    pool: {afterCreate: (conn , done)=> conn.run('PRAGMA foreign_keys = ON', done)}
+    pool: {afterCreate: (conn , done)=> conn.run('PRAGMA foreign_keys = ON', done)},
 }
 
 module.exports = {
     development: {
         ...sharedConfig,
-        connnection: { filename: './data/cook_book.db3'},
+        connection: { filename: './data/cook_book.db3'},
     },
     testing: {
         ...sharedConfig,
-        connnection: {filename: './data/cook_book.test.db3'}
+        connection: {filename: './data/cook_book.test.db3'}
     },
     production: {
     }
